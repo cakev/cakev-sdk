fbcb8c8a763b8bc14e4d8ff58e70bb7a603aecca
zhepei.shen <zhepei.shen@eslink.com>
2022-05-25 14:00:31 +0800

    feat: 🎸 type update

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -2,11 +2,5 @@ export {}
 declare global {
 	interface window {
 		G2: any
-		cakeVEditorInstance: any
-		cakeV: any
-	}
-	export class MyMouseEvent extends MouseEvent {
-		readonly layerX: number
-		readonly layerY: number
 	}
 }
